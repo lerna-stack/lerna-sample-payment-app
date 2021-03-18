a2ec04ca43629ecd60eace16a9cdcda0fec16b34
杉本 貴史 <sugimoto.takashi@tis.co.jp>
2021-03-18 18:57:13 +0900

    docs: numberOfTags についての注意事項をコメントとして記述

--- payment-app/application/src/main/scala/jp/co/tis/lerna/payment/application/readmodelupdater/HasDomainEventTag.scala
+++ payment-app/application/src/main/scala/jp/co/tis/lerna/payment/application/readmodelupdater/HasDomainEventTag.scala
@@ -5,6 +5,8 @@ trait HasDomainEventTag {
   def domainEventTagPrefix: String
 
   /** Read Model Updater を何並列で動かすか
+    * <p> ※ 基本的には一度動かしたら途中で変更しない。Shard数と同じようにクラスターノードの計画された最大数の10倍程度が推奨されている。<br>
+    * 参考: [[https://doc.akka.io/docs/akka-projection/current/running.html#tagging-events-in-eventsourcedbehavior Running a Projection • Akka Projection]]
     */
   def numberOfTags: Int
 
