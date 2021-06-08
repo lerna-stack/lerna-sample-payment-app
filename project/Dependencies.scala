5e29e4df9da68547641762bb856c60716d2a810a
杉本 貴史 <sugimoto.takashi@tis.co.jp>
2021-06-08 17:55:09 +0900

    chore: Update lerna-app-library to 2.0.0-92f060be-SNAPSHOT
    - ScalaTest v3.1.4 対応
    - lerna-util-akka typed 対応

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val lerna                    = "2.0.0-6bad8983-SNAPSHOT"
+    val lerna                    = "2.0.0-92f060be-SNAPSHOT"
     val akka                     = "2.6.10"
     val akkaHttp                 = "10.1.12"
     val akkaPersistenceCassandra = "1.0.1"
