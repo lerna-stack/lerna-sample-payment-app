7f430b9db32ecdddc62525d863ce9bdb6d7beb41
杉本 貴史 <sugimoto.takashi@tis.co.jp>
2021-06-16 17:33:15 +0900

    refactor: slick-codegen/scalafmtAll
    Scala version up 関係無い

--- slick-codegen/src/main/scala/Codegen.scala
+++ slick-codegen/src/main/scala/Codegen.scala
@@ -33,10 +33,10 @@ object Codegen extends App {
   // fetch data model
   val model = for {
     modelAction <-
-    // HACK カスタマイズした CustomJdbcModelBuilder を使います。詳しい経緯は README を参照してください。
-    // profile.createModel(Option(tables), ignoreInvalidDefaults = false) // you can filter specific tables here
-    new CustomJdbcModelComponentExt(profile.asInstanceOf[MySQLProfile])
-      .createModel(Option(tables), ignoreInvalidDefaults = false)
+      // HACK カスタマイズした CustomJdbcModelBuilder を使います。詳しい経緯は README を参照してください。
+      // profile.createModel(Option(tables), ignoreInvalidDefaults = false) // you can filter specific tables here
+      new CustomJdbcModelComponentExt(profile.asInstanceOf[MySQLProfile])
+        .createModel(Option(tables), ignoreInvalidDefaults = false)
   } yield modelAction
 
   val modelFuture = db.run(model)
@@ -46,13 +46,13 @@ object Codegen extends App {
     .map { model =>
       new SourceCodeGenerator(model) {
 
-        /**
-          * デフォルトからの変更点
+        /** デフォルトからの変更点
           * - slick.collection.heterogeneous.syntax のインポートを除去
           * - slick.model.ForeignKeyAction のインポートを除去
           */
         /** Generates code for the complete model (not wrapped in a package yet)
-      @group Basic customization overrides */
+          *      @group Basic customization overrides
+          */
         override def code = {
           // "import slick.model.ForeignKeyAction\n" +
           (if (tables.exists(table => table.hlistEnabled || table.isMappedToHugeClass)) {
@@ -67,8 +67,7 @@ object Codegen extends App {
           tables.map(_.code.mkString("\n")).mkString("\n\n")
         }
 
-        /**
-          * デフォルトからの変更点
+        /** デフォルトからの変更点
           * - Tables object を生成しない
           * - Schema を未指定（None）に変更
           */
@@ -102,7 +101,7 @@ object Codegen extends App {
               val args = model.name.schema.map(_ => s"""None""") ++ Seq("\"" + model.name.table + "\"")
               s"""
               |class $name(_tableTag: Tag) extends profile.api.Table[$elementType](_tableTag, ${args
-                   .mkString(", ")})$prns {
+                .mkString(", ")})$prns {
               |  ${indent(body.map(_.mkString("\n")).mkString("\n\n"))}
               |}
               """.stripMargin
