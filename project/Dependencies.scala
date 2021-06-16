2842f9d461855c5d1a7908e5997f32ca7977221f
杉本 貴史 <sugimoto.takashi@tis.co.jp>
2021-06-16 15:57:30 +0900

    chore: Update lerna-app-library 2.0.0-80f86b49-SNAPSHOT
    - Akka-HTTP 10.2.4 対応
    - 他

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val lerna                    = "2.0.0-f8684d32-SNAPSHOT"
+    val lerna                    = "2.0.0-80f86b49-SNAPSHOT"
     val akka                     = "2.6.10"
     val akkaHttp                 = "10.1.12"
     val akkaPersistenceCassandra = "1.0.1"
