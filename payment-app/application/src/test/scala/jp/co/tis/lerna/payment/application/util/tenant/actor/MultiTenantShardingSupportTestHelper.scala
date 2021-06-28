0e4effed47a0f729f417c1df78e179f584d12d9f
杉本 貴史 <sugimoto.takashi@tis.co.jp>
2021-06-28 18:30:20 +0900

    refactor: deprecated な Stream の使用を廃止
    > [warn]       value Stream in package scala is deprecated (since 2.13.0): Use LazyList instead of Stream
    
    ついでにスレッドセーフな AtomicInteger に切り替え

--- payment-app/application/src/test/scala/jp/co/tis/lerna/payment/application/util/tenant/actor/MultiTenantShardingSupportTestHelper.scala
+++ payment-app/application/src/test/scala/jp/co/tis/lerna/payment/application/util/tenant/actor/MultiTenantShardingSupportTestHelper.scala
@@ -3,13 +3,15 @@ package jp.co.tis.lerna.payment.application.util.tenant.actor
 import akka.cluster.sharding.ShardRegion.EntityId
 import jp.co.tis.lerna.payment.utility.tenant.AppTenant
 
+import java.util.concurrent.atomic.AtomicInteger
+
 object MultiTenantShardingSupportTestHelper {
   def generateEntityId()(implicit tenant: AppTenant): EntityId = {
     MultiTenantShardingSupport.tenantSupportEntityId(s"${generateUniqueNumber().toString}")
   }
 
   private[this] val generateUniqueNumber: () => Int = {
-    val iterator = Stream.from(0).iterator
-    () => iterator.next()
+    val counter = new AtomicInteger(0)
+    () => counter.incrementAndGet()
   }
 }
