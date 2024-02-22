6cd13ec9f86f8e9250c4ac45009e986a49f1264e
Naoki Yamada <yamada.naoki@tis.co.jp>
2024-02-22 14:41:16 +0900

    wip

--- payment-app/application/src/test/scala/jp/co/tis/lerna/payment/application/ecpayment/issuing/actor/KryoSerializationCompatibilityTest.scala
+++ payment-app/application/src/test/scala/jp/co/tis/lerna/payment/application/ecpayment/issuing/actor/KryoSerializationCompatibilityTest.scala
@@ -110,19 +110,22 @@ class PekkoSerializationTest extends SpecAssertions with AnyFunSuiteLike {
 
   test("Deserialize ActorRef in Pekko") {
     val event = ActorRefSerialize(actorRef)
-    // ActorRefSerialize(akka://PekkoSerializationTest/user/foo#2002623825)をkryoでシリアライズしたもの
-    val serialized: Array[Byte] = Array(1,0,-50,1,106,112,46,99,111,46,116,105,115,46,108,101,114,110,97,46,112,97,121,109,101,110,116,46,97,112,112,108,105,99,97,116,105,111,110,46,101,99,112,97,121,109,101,110,116,46,105,115,115,117,105,110,103,46,97,99,116,111,114,46,65,99,116,111,114,82,101,102,83,101,114,105,97,108,105,122,101,1,1,1,97,107,107,97,46,97,99,116,111,114,46,82,101,112,111,105,110,116,97,98,108,101,65,99,116,111,114,82,101,-26,1,97,107,107,97,58,47,47,80,101,107,107,111,83,101,114,105,97,108,105,122,97,116,105,111,110,84,101,115,116,47,117,115,101,114,47,102,111,111,35,50,48,48,50,54,50,51,56,50,-75)
-    println(new String(serialized, StandardCharsets.UTF_8))
+    // v1.1.5でシリアライズ ActorRefSerialize(akka://PekkoSerializationTest/user/foo#2002623825)をkryoでシリアライズしたもの
+    val serialized_by_v1: Array[Byte] = Array(1,0,-50,1,106,112,46,99,111,46,116,105,115,46,108,101,114,110,97,46,112,97,121,109,101,110,116,46,97,112,112,108,105,99,97,116,105,111,110,46,101,99,112,97,121,109,101,110,116,46,105,115,115,117,105,110,103,46,97,99,116,111,114,46,65,99,116,111,114,82,101,102,83,101,114,105,97,108,105,122,101,1,1,1,97,107,107,97,46,97,99,116,111,114,46,82,101,112,111,105,110,116,97,98,108,101,65,99,116,111,114,82,101,-26,1,97,107,107,97,58,47,47,80,101,107,107,111,83,101,114,105,97,108,105,122,97,116,105,111,110,84,101,115,116,47,117,115,101,114,47,102,111,111,35,50,48,48,50,54,50,51,56,50,-75)
+    // v2.5.0でシリアライズ ActorRefSerialize(Actor[akka://PekkoSerializationTest/user/foo#-1604754461])
+    val serialized_by_v2: Array[Byte] = Array(1,0,106,112,46,99,111,46,116,105,115,46,108,101,114,110,97,46,112,97,121,109,101,110,116,46,97,112,112,108,105,99,97,116,105,111,110,46,101,99,112,97,121,109,101,110,116,46,105,115,115,117,105,110,103,46,97,99,116,111,114,46,65,99,116,111,114,82,101,102,83,101,114,105,97,108,105,122,-27,1,1,1,97,107,107,97,46,97,99,116,111,114,46,82,101,112,111,105,110,116,97,98,108,101,65,99,116,111,114,82,101,-26,1,97,107,107,97,58,47,47,80,101,107,107,111,83,101,114,105,97,108,105,122,97,116,105,111,110,84,101,115,116,47,117,115,101,114,47,102,111,111,35,45,49,54,48,52,55,53,52,52,54,-79)
     val serializer   = pekkoSerializationExtension.findSerializerFor(event)
     val serializerId = serializer.identifier
     val manifest     = classOf[ActorRefSerialize].getName
-    val deserialized = pekkoSerializationExtension.deserialize(serialized, serializerId, manifest).get
-    expect(deserialized.toString === "ActorRefSerialize(Actor[pekko://PekkoSerializationTest/user/foo#2002623825])")
+    val deserialized_1 = pekkoSerializationExtension.deserialize(serialized_by_v1, serializerId, manifest).get
+    val deserialized_2 = pekkoSerializationExtension.deserialize(serialized_by_v2, serializerId, manifest).get
+    expect(deserialized_1.toString === "ActorRefSerialize(Actor[pekko://PekkoSerializationTest/user/foo#2002623825])")
+    expect(deserialized_2.toString === "ActorRefSerialize(Actor[pekko://PekkoSerializationTest/user/foo#-1604754461])")
   }
 
   test("Deserialize typed ActorRef in Pekko") {
     val event = TypedActorRefSerialize(actorRef.toTyped)
-    // TypedActorRefSerialize(Actor[pekko://PekkoSerializationTest/user/foo#143757351])をシリアライズしたもの
+    // v2.5.0 でシリアライズ TypedActorRefSerialize(Actor[pekko://PekkoSerializationTest/user/foo#143757351])をシリアライズしたもの
     val serialized: Array[Byte] = Array(1, 0, 106, 112, 46, 99, 111, 46, 116, 105, 115, 46, 108, 101, 114, 110, 97, 46, 112, 97, 121, 109, 101, 110, 116, 46, 97, 112, 112, 108, 105, 99, 97, 116, 105, 111, 110, 46, 101, 99, 112, 97, 121, 109, 101, 110, 116, 46, 105, 115, 115, 117, 105, 110, 103, 46, 97, 99, 116, 111, 114, 46, 84, 121, 112, 101, 100, 65, 99, 116, 111, 114, 82, 101, 102, 83, 101, 114, 105, 97, 108, 105, 122, -27, 1, 1, 1, 97, 107, 107, 97, 46, 97, 99, 116, 111, 114, 46, 116, 121, 112, 101, 100, 46, 105, 110, 116, 101, 114, 110, 97, 108, 46, 97, 100, 97, 112, 116, 101, 114, 46, 65, 99, 116, 111, 114, 82, 101, 102, 65, 100, 97, 112, 116, 101, -14, 1, 97, 107, 107, 97, 58, 47, 47, 80, 101, 107, 107, 111, 83, 101, 114, 105, 97, 108, 105, 122, 97, 116, 105, 111, 110, 84, 101, 115, 116, 47, 117, 115, 101, 114, 47, 102, 111, 111, 35, 49, 52, 51, 55, 53, 55, 51, 53, -79)
     val serializer   = pekkoSerializationExtension.findSerializerFor(event)
     val serializerId = serializer.identifier
@@ -136,15 +139,18 @@ class PekkoSerializationTest extends SpecAssertions with AnyFunSuiteLike {
     val event        = ByteStringSerialize(ByteString(""))
     val serializer   = pekkoSerializationExtension.findSerializerFor(event)
     val serializerId = serializer.identifier
-    println(serializerId)
 
     // ByteStringSerialize("foo") をakkaでシリアライズしたバイト列
-    val serialized: Array[Byte] = Array(1,0,-48,1,106,112,46,99,111,46,116,105,115,46,108,101,114,110,97,46,112,97,121,109,101,110,116,46,97,112,112,108,105,99,97,116,105,111,110,46,101,99,112,97,121,109,101,110,116,46,105,115,115,117,105,110,103,46,97,99,116,111,114,46,66,121,116,101,83,116,114,105,110,103,83,101,114,105,97,108,105,122,101,1,1,1,97,107,107,97,46,117,116,105,108,46,66,121,116,101,83,116,114,105,110,103,36,66,121,116,101,83,116,114,105,110,103,49,-61,1,3,102,111,111)
+    // v1.1.5でシリアライズ
+    val serialized_by_v1: Array[Byte] = Array(1,0,-48,1,106,112,46,99,111,46,116,105,115,46,108,101,114,110,97,46,112,97,121,109,101,110,116,46,97,112,112,108,105,99,97,116,105,111,110,46,101,99,112,97,121,109,101,110,116,46,105,115,115,117,105,110,103,46,97,99,116,111,114,46,66,121,116,101,83,116,114,105,110,103,83,101,114,105,97,108,105,122,101,1,1,1,97,107,107,97,46,117,116,105,108,46,66,121,116,101,83,116,114,105,110,103,36,66,121,116,101,83,116,114,105,110,103,49,-61,1,3,102,111,111)
+    // v2.5.0でシリアライズ
+    val serialized_by_v2: Array[Byte] = Array(1,0,106,112,46,99,111,46,116,105,115,46,108,101,114,110,97,46,112,97,121,109,101,110,116,46,97,112,112,108,105,99,97,116,105,111,110,46,101,99,112,97,121,109,101,110,116,46,105,115,115,117,105,110,103,46,97,99,116,111,114,46,66,121,116,101,83,116,114,105,110,103,83,101,114,105,97,108,105,122,-27,1,1,1,97,107,107,97,46,117,116,105,108,46,66,121,116,101,83,116,114,105,110,103,36,66,121,116,101,83,116,114,105,110,103,49,-61,1,3,102,111,111)
 
     val manifest     = classOf[ByteStringSerialize].getName
-    val deserialized = pekkoSerializationExtension.deserialize(serialized, serializerId, manifest).get
-    println(deserialized)
-    expect(deserialized === actor.ByteStringSerialize(ByteString("foo")))
+    val deserialized_1 = pekkoSerializationExtension.deserialize(serialized_by_v1, serializerId, manifest).get
+    val deserialized_2 = pekkoSerializationExtension.deserialize(serialized_by_v2, serializerId, manifest).get
+    expect(deserialized_1 === actor.ByteStringSerialize(ByteString("foo")))
+    expect(deserialized_2 === actor.ByteStringSerialize(ByteString("foo")))
   }
 
   object Foo {
